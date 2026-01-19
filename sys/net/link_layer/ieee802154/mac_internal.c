3fda30c4e4ccf0323cbbcaa83e75fecf832fadb2
= <=>
2026-01-19 21:17:01 +0100

    ieee802154/mac: moved queue functions out of the header

--- sys/net/link_layer/ieee802154/mac_internal.c
+++ sys/net/link_layer/ieee802154/mac_internal.c
@@ -52,6 +52,316 @@ extern void auto_init_event_thread(void);
                      IS_USED(MODULE_KW2XRF) + \
                      IS_USED(MODULE_ESP_IEEE802154)
 
+static uint8_t ieee80214_addr_len_from_mode(ieee802154_addr_mode_t mode)
+{
+    switch (mode) {
+    case IEEE802154_ADDR_MODE_NONE:
+        return 0;
+    case IEEE802154_ADDR_MODE_SHORT:
+        return IEEE802154_SHORT_ADDRESS_LEN;
+    case IEEE802154_ADDR_MODE_EXTENDED:
+        return IEEE802154_LONG_ADDRESS_LEN;
+    default:
+        return 0;
+    }
+}
+
+bool ieee802154_mac_tx_full(const ieee802154_mac_txq_t *txq)
+{
+    return txq->cnt >= IEEE802154_MAC_TXQ_LEN;
+}
+
+bool ieee802154_mac_tx_empty(const ieee802154_mac_txq_t *txq)
+{
+    return (txq->cnt == 0);
+}
+
+ieee802154_mac_tx_desc_t *ieee802154_mac_tx_reserve(ieee802154_mac_txq_t *txq)
+{
+    if (!txq || ieee802154_mac_tx_full(txq)) {
+        return NULL;
+    }
+
+    ieee802154_mac_tx_desc_t *d = &txq->q[txq->tail];
+    memset(d, 0, sizeof(*d));
+    d->in_use = true;
+    return d;
+}
+
+void ieee802154_mac_tx_commit(ieee802154_mac_txq_t *txq)
+{
+    txq->tail = (uint8_t)((txq->tail + 1) % IEEE802154_MAC_TXQ_LEN);
+    txq->cnt++;
+}
+
+ieee802154_mac_tx_desc_t *ieee802154_mac_tx_peek(ieee802154_mac_txq_t *txq)
+{
+    if (!txq || ieee802154_mac_tx_empty(txq)) {
+        return NULL;
+    }
+
+    ieee802154_mac_tx_desc_t *d = &txq->q[txq->head];
+
+    if (!d->in_use) {
+        return NULL;
+    }
+
+    return d;
+}
+
+void ieee802154_mac_tx_pop(ieee802154_mac_txq_t *txq)
+{
+    if (!txq || ieee802154_mac_tx_empty(txq)) {
+        return;
+    }
+
+    ieee802154_mac_tx_desc_t *d = &txq->q[txq->head];
+    memset(d, 0, sizeof(*d));
+
+    txq->head = (uint8_t)((txq->head + 1) % IEEE802154_MAC_TXQ_LEN);
+    txq->cnt--;
+}
+
+int ieee802154_indirectq_alloc_slot(ieee802154_mac_indirect_q_t *indirect_q)
+{
+    if (indirect_q->free_mask == 0) {
+        return -1;
+    }
+
+    /* first free slot */
+    uint8_t slot = __builtin_ctz(indirect_q->free_mask);
+    /* 0 means used */
+    indirect_q->free_mask &= ~(1U << slot);
+    return slot;
+}
+
+void ieee802154_indirectq_free_slot(ieee802154_mac_indirect_q_t *indirect_q, uint8_t slot)
+{
+    memset(&indirect_q->q[slot], 0, sizeof(ieee802154_mac_txq_t));
+    indirect_q->free_mask |= (1U << slot);
+}
+
+uint16_t ieee802154_indirect_get_deadline(ieee802154_mac_t *mac)
+{
+    ieee802154_pib_value_t value;
+    ieee802154_mac_mlme_get(mac, IEEE802154_PIB_TRANSACTION_PERSISTENCE_TIME, &value);
+    uint16_t unit_period_us = value.v.u16 * mac->sym_us;
+    /* round up to handle too early timeouts */
+    uint16_t unit_period_ticks =
+        (unit_period_us + IEEE802154_MAC_TICK_INTERVAL_US - 1U) / IEEE802154_MAC_TICK_INTERVAL_US;
+    return (mac->indirect_q.tick + (unit_period_ticks * value.v.u16));
+}
+
+bool ieee802154_mac_frame_is_expired(uint16_t now_tick, uint16_t deadline_tick)
+{
+    return (bool)((now_tick - deadline_tick) >= 0);
+}
+
+void ieee802154_mac_indirect_fp_update(ieee802154_mac_t *mac,
+                                       const ieee802154_ext_addr_t *dst_addr,
+                                       bool pending)
+{
+#ifdef IEEE802154_MAC_INDIRECT_ENABLE
+    ieee802154_dev_t *dev = &mac->submac.dev;
+#  ifdef IEEE802154_MAC_HAS_SRC_ADDR_MATCH
+    ieee802154_radio_config_src_address_match(dev,
+                                              pending ? IEEE802154_SRC_MATCH_EXT_ADD
+                                                      : IEEE802154_SRC_MATCH_EXT_CLEAR,
+                                              dst_addr);
+#  else
+    (void)dst_addr;
+    bool any_pending = pending;
+    if (!pending) {
+        uint32_t full_mask = (1U << IEEE802154_MAC_TX_INDIRECTQ_SIZE) - 1U;
+        any_pending = (mac->indirect_q.free_mask != full_mask);
+    }
+    printf("pending %u", any_pending);
+    ieee802154_radio_config_src_address_match(dev, IEEE802154_SRC_MATCH_EN, &any_pending);
+#  endif
+#else
+    (void)mac;
+    (void)dst_addr;
+    (void)pending;
+#endif
+}
+
+void ieee802154_mac_handle_indirectq_auto_free(ieee802154_mac_t *mac,
+                                               ieee802154_mac_indirect_q_t *indirect_q,
+                                               uint8_t slot)
+{
+    ieee802154_mac_txq_t *txq = &indirect_q->q[slot];
+    if (ieee802154_mac_tx_empty(txq)) {
+        ieee802154_ext_addr_t dst_addr = txq->dst_addr;
+        ieee802154_indirectq_free_slot(indirect_q, slot);
+        ieee802154_mac_indirect_fp_update(mac, &dst_addr, false);
+    }
+    else {
+        ieee802154_mac_tx_desc_t *d = ieee802154_mac_tx_peek(txq);
+        if (d) {
+            txq->deadline_tick = &d->deadline_tick;
+        }
+    }
+}
+
+static int _enqueue_data_tx(ieee802154_mac_t *mac,
+                            uint8_t frame_type,
+                            ieee802154_mac_txq_t *txq,
+                            ieee802154_addr_mode_t src_mode,
+                            ieee802154_addr_mode_t dst_mode,
+                            const uint16_t *dst_panid,
+                            const void *dst_addr,
+                            iolist_t *msdu,
+                            const uint8_t *msdu_handle,
+                            bool ack_req,
+                            bool indirect)
+{
+    DEBUG("Enqeue data in tx\n");
+    ieee802154_mac_tx_desc_t *dsc = ieee802154_mac_tx_reserve(txq);
+    if (!dsc) {
+        return -ENOBUFS;
+    }
+    dsc->handle = *msdu_handle;
+    dsc->ack = ack_req;
+    dsc->indirect = indirect;
+
+    /* src addr selection */
+    const void *src = NULL;
+    ieee802154_pib_value_t src_v;
+    if (src_mode == IEEE802154_ADDR_MODE_SHORT) {
+        ieee802154_mac_mlme_get(mac, IEEE802154_PIB_SHORT_ADDR, &src_v);
+        src = &src_v.v.short_addr;
+    }
+    else if (src_mode == IEEE802154_ADDR_MODE_EXTENDED) {
+        ieee802154_mac_mlme_get(mac, IEEE802154_PIB_EXTENDED_ADDRESS, &src_v);
+        src = &src_v.v.ext_addr;
+    }
+
+    uint8_t src_len = ieee80214_addr_len_from_mode(src_mode);
+    uint8_t dst_len = ieee80214_addr_len_from_mode(dst_mode);
+    ieee802154_pib_value_t src_panid;
+    ieee802154_mac_mlme_get(mac, IEEE802154_PIB_PAN_ID, &src_panid);
+    le_uint16_t src_pan = byteorder_btols(byteorder_htons(src_panid.v.u16));
+    le_uint16_t dst_pan = byteorder_btols(byteorder_htons(*dst_panid));
+
+    ieee802154_pib_value_t dsn;
+    ieee802154_mac_mlme_get(mac, IEEE802154_PIB_DSN, &dsn);
+
+    uint8_t flags = frame_type;
+    if (ack_req) {
+        flags |= IEEE802154_FCF_ACK_REQ;
+    }
+
+    bool was_empty = ieee802154_mac_tx_empty(txq);
+    size_t mhr_len = ieee802154_set_frame_hdr(dsc->mhr,
+                                              src, (size_t)src_len,
+                                              dst_len ? dst_addr : NULL, (size_t)dst_len,
+                                              src_pan, dst_pan,
+                                              flags,
+                                              dsn.v.u8);
+    if (mhr_len == 0 || mhr_len > (int)sizeof(dsc->mhr)) {
+        dsc->in_use = false;
+        return -EINVAL;
+    }
+
+    dsc->iol_msdu = msdu;
+    dsc->iol_mhr.iol_base = &dsc->mhr;
+    dsc->iol_mhr.iol_len = mhr_len;
+    dsc->iol_mhr.iol_next = dsc->iol_msdu;
+
+    /* DSN++ */
+    ieee802154_pib_value_t dsn_new = {
+        .type = IEEE802154_PIB_TYPE_U8,
+        .v.u8 = dsn.v.u8 + 1,
+    };
+    uint16_t deadline = ieee802154_indirect_get_deadline(mac);
+    dsc->deadline_tick = deadline;
+    if (was_empty) {
+        txq->deadline_tick = &dsc->deadline_tick;
+    }
+    ieee802154_mac_tx_commit(txq);
+    if (indirect && (dst_mode == IEEE802154_ADDR_MODE_EXTENDED)) {
+        ieee802154_mac_indirect_fp_update(mac, (const ieee802154_ext_addr_t *)dst_addr, true);
+    }
+    ieee802154_mac_mlme_set(mac, IEEE802154_PIB_DSN, &dsn_new);
+    return 0;
+}
+
+int ieee802154_mac_indirectq_search_slot(ieee802154_mac_indirect_q_t *indirect_q,
+                                         const ieee802154_ext_addr_t *dst_addr)
+{
+    if (!dst_addr) {
+        return -1;
+    }
+    for (int i = 0; i < IEEE802154_MAC_TX_INDIRECTQ_SIZE; i++) {
+        if (memcmp(indirect_q->q[i].dst_addr.uint8,
+                   dst_addr->uint8,
+                   IEEE802154_LONG_ADDRESS_LEN) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int ieee802154_mac_indirectq_get_slot(ieee802154_mac_indirect_q_t *indirect_q,
+                                      const ieee802154_ext_addr_t *dst_addr)
+{
+    int slot = ieee802154_mac_indirectq_search_slot(indirect_q, dst_addr);
+    if (slot >= 0) {
+        return slot;
+    }
+    slot = ieee802154_indirectq_alloc_slot(indirect_q);
+    if (slot < 0) {
+        return slot;
+    }
+    indirect_q->q[slot].dst_addr = *dst_addr;
+    return slot;
+}
+
+int ieee802154_mac_map_push(ieee802154_mac_t *mac,
+                            uint8_t frame_type,
+                            ieee802154_addr_mode_t src_mode,
+                            ieee802154_addr_mode_t dst_mode,
+                            uint16_t *dst_panid,
+                            const void *dst_addr,
+                            iolist_t *msdu,
+                            const uint8_t *msdu_handle,
+                            bool ack_req,
+                            bool indirect)
+{
+    // TODO: mapping short to extended
+    if ((dst_mode != IEEE802154_ADDR_MODE_EXTENDED) || (dst_addr == NULL)) {
+        return -EINVAL;
+    }
+    int slot = ieee802154_mac_indirectq_get_slot(&mac->indirect_q, dst_addr);
+    if (slot < 0) {
+        return -ENOBUFS;
+    }
+    int res = _enqueue_data_tx(mac, frame_type, &mac->indirect_q.q[slot], src_mode,
+                               dst_mode, dst_panid, dst_addr, msdu,
+                               msdu_handle, ack_req, indirect);
+    if (res < 0) {
+        DEBUG("Enqueue Data failed\n");
+        return res;
+    }
+    DEBUG("Frame enqueued \n");
+    return 0;
+}
+
+int ieee802154_mac_enqueue_data_request(ieee802154_mac_t *mac,
+                                        ieee802154_addr_mode_t dst_mode,
+                                        uint16_t *dst_panid,
+                                        const void *dst_addr)
+{
+    *(uint8_t *)mac->cmd.iol_base = IEEE802154_CMD_DATA_REQ;
+    mac->cmd.iol_len = 1;
+    mac->cmd.iol_next = NULL;
+    // TODO: check for src addressing mode
+    uint8_t handle = 0xFFU;
+    ieee802154_mac_map_push(mac, IEEE802154_FCF_TYPE_MACCMD, dst_mode, dst_mode,
+                            dst_panid, dst_addr, &mac->cmd, &handle, true, false);
+    return 0;
+}
+
 
 #ifdef MODULE_KW2XRF
 #  include "kw2xrf.h"
