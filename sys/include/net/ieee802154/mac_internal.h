4bfdbd86410569052876d7c61579c45588557ce8
Moritz Holzer <moritz.holzer@haw-hamburg.de>
2026-01-19 23:47:50 +0100

    ieee802154/mac: added some comments

--- sys/include/net/ieee802154/mac_internal.h
+++ sys/include/net/ieee802154/mac_internal.h
@@ -38,23 +38,29 @@ typedef enum {
     IEEE802154_MAC_EV_TX,               /**< wakeup event */
 } ieee802154_mac_ev_t;
 
+/**
+ * @brief Initialize internal MAC state.
+ */
 void ieee802154_init_mac_internal(ieee802154_mac_t *mac);
 
 /**
- * @brief Attaches the IEEE 802.15.4 SubMAC callbacks.
+ * @brief Attach the IEEE 802.15.4 SubMAC callbacks.
  */
 void ieee802154_mac_submac_attach(ieee802154_mac_t *mac);
 
 /**
- * @brief Attaches the IEEE 802.15.4 SubMAC callbacks.
+ * @brief Finish the current TX and report status.
  */
 void ieee802154_mac_tx_finish_current(ieee802154_mac_t *mac, int status);
 
 /**
- * @brief Attaches the IEEE 802.15.4 Radio HAL callbacks.
+ * @brief Attach the IEEE 802.15.4 Radio HAL callbacks.
  */
 void ieee802154_mac_radio_attach(ieee802154_mac_t *mac);
 
+/**
+ * @brief Transmit a MAC frame to the given destination.
+ */
 int ieee802154_mac_tx(ieee802154_mac_t *mac, const ieee802154_ext_addr_t *dst_addr);
 
 /**
@@ -87,20 +93,47 @@ ieee802154_mac_tx_desc_t *ieee802154_mac_tx_peek(ieee802154_mac_txq_t *txq);
  */
 void ieee802154_mac_tx_pop(ieee802154_mac_txq_t *txq);
 
+/**
+ * @brief Allocate a slot in the indirect queue.
+ */
 int ieee802154_indirectq_alloc_slot(ieee802154_mac_indirect_q_t *indirect_q);
+/**
+ * @brief Free a slot in the indirect queue.
+ */
 void ieee802154_indirectq_free_slot(ieee802154_mac_indirect_q_t *indirect_q, uint8_t slot);
+/**
+ * @brief Get the deadline tick for indirect transmission.
+ */
 uint16_t ieee802154_indirect_get_deadline(ieee802154_mac_t *mac);
+/**
+ * @brief Check whether a frame is expired.
+ */
 bool ieee802154_mac_frame_is_expired(uint16_t now_tick, uint16_t deadline_tick);
+/**
+ * @brief Update the frame pending bit for a destination address.
+ */
 void ieee802154_mac_indirect_fp_update(ieee802154_mac_t *mac,
                                        const ieee802154_ext_addr_t *dst_addr,
                                        bool pending);
+/**
+ * @brief Auto-free an indirect queue slot when it expires.
+ */
 void ieee802154_mac_handle_indirectq_auto_free(ieee802154_mac_t *mac,
                                                ieee802154_mac_indirect_q_t *indirect_q,
                                                uint8_t slot);
+/**
+ * @brief Find an indirect queue slot by destination address.
+ */
 int ieee802154_mac_indirectq_search_slot(ieee802154_mac_indirect_q_t *indirect_q,
                                          const ieee802154_ext_addr_t *dst_addr);
+/**
+ * @brief Get a slot index for the given destination address.
+ */
 int ieee802154_mac_indirectq_get_slot(ieee802154_mac_indirect_q_t *indirect_q,
                                       const ieee802154_ext_addr_t *dst_addr);
+/**
+ * @brief Enqueue a data frame into the MAC TX queue.
+ */
 int ieee802154_mac_map_push(ieee802154_mac_t *mac,
                             uint8_t frame_type,
                             ieee802154_addr_mode_t src_mode,
@@ -111,6 +144,9 @@ int ieee802154_mac_map_push(ieee802154_mac_t *mac,
                             const uint8_t *msdu_handle,
                             bool ack_req,
                             bool indirect);
+/**
+ * @brief Enqueue a data request command.
+ */
 int ieee802154_mac_enqueue_data_request(ieee802154_mac_t *mac,
                                         ieee802154_addr_mode_t dst_mode,
                                         uint16_t *dst_panid,
