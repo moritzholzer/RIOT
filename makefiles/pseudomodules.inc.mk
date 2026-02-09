f496105e0a88afaa3c604b2ebbd301498175059a
Moritz Holzer <moritz.holzer@haw-hamburg.de>
2026-02-09 22:46:43 +0100

    sys/shelll/cmds: added iwpan command for ieee802154_mac netif

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -423,6 +423,7 @@ PSEUDOMODULES += shell_cmd_gnrc_udp
 PSEUDOMODULES += shell_cmd_heap
 PSEUDOMODULES += shell_cmd_i2c_scan
 PSEUDOMODULES += shell_cmd_iw
+PSEUDOMODULES += shell_cmd_iwpan
 PSEUDOMODULES += shell_cmd_lwip_netif
 PSEUDOMODULES += shell_cmd_mci
 PSEUDOMODULES += shell_cmd_md5sum
