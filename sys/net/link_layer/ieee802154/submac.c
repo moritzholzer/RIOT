aa1cf6a24b930cbef51ad7c0286b7154580f46d1
Moritz Holzer <moritz.holzer@haw-hamburg.de>
2026-02-13 15:34:22 +0100

    sys/net/link_layer/ieee802154: added frame pending to info

--- sys/net/link_layer/ieee802154/submac.c
+++ sys/net/link_layer/ieee802154/submac.c
@@ -368,6 +368,7 @@ static ieee802154_fsm_state_t _fsm_state_tx_process_tx_done(ieee802154_submac_t 
     switch (info->status) {
     case TX_STATUS_FRAME_PENDING:
         assert(_does_handle_ack(dev));
+        info->frame_pending = true;
     /* FALL-THRU */
     case TX_STATUS_SUCCESS:
         submac->csma_retries_nb = 0;
@@ -455,10 +456,10 @@ static ieee802154_fsm_state_t _fsm_state_wait_for_ack(ieee802154_submac_t *subma
             ieee802154_submac_ack_timer_cancel(submac);
             ieee802154_tx_info_t tx_info;
             tx_info.retrans = submac->retrans;
-            bool fp = (ack[0] & IEEE802154_FCF_FRAME_PEND);
+            tx_info.frame_pending = (ack[0] & IEEE802154_FCF_FRAME_PEND);
             ieee802154_radio_set_frame_filter_mode(&submac->dev, IEEE802154_FILTER_ACCEPT);
-            return _tx_end(submac, fp ? TX_STATUS_FRAME_PENDING : TX_STATUS_SUCCESS,
-                           &tx_info);
+            DEBUG("IEEE802154 submac: filter -> ACCEPT (ACK received)\n");
+            return _tx_end(submac, TX_STATUS_SUCCESS, &tx_info);
         }
         return IEEE802154_FSM_STATE_WAIT_FOR_ACK;
     case IEEE802154_FSM_EV_CRC_ERROR:
